db5082de3f58eb0868b9a94ea0e98b5a44aa09df
chriswolfdesign <chriswolfdesign@gmail.com>
2020-01-05 14:43:31 -0500

    Added documentation to NavBarLinkButton

--- src/components/NavBarLinkButton.js
+++ src/components/NavBarLinkButton.js
@@ -1,12 +1,24 @@
 import React, { Component } from "react";
 import {BrowserRouter as Router, Link} from 'react-router-dom';
 
+/**
+ * NavBarLinkButton.js
+ * 
+ * A component that exists in our header and is used to take us somewhere 
+ * either inside or outside of our application
+ * 
+ * @author Chris Wolf
+ * @version 1.0.0 (January 5, 2020)
+ */
+
 export class NavBarLinkButton extends Component {
   
+  // the state of our NavBarLinkButton
   state = {
     hovered: false
   }
 
+  // generates the style for NavBarLinkButton
   getStyle = () => {
     return {
       cursor: "pointer",
@@ -22,12 +34,14 @@ export class NavBarLinkButton extends Component {
     }
   }
 
+  // toggles whether or not this NavBarLinkButton is being hovered over
   toggleHover = () => {
     this.setState({
       hovered: !this.state.hovered
     });
   }
   
+  // generates the JSX for this NavBarLinkButton
   render() {
     return (
       <div
@@ -42,6 +56,7 @@ export class NavBarLinkButton extends Component {
   }
 }
 
+// the style for the embedded links
 const linkStyle = {
   color: 'white',
   textDecoration: 'none'
